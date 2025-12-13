d755074a9a32da315d770970675f6b5231ff5749
Quinntyne Brown <quinntynebrown@gmail.com>
2025-12-13 15:36:33 -0500

    update

--- src/ZoomLoop.App/src/app/app.ts
+++ src/ZoomLoop.App/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { FooterComponent, NavbarComponent } from './components';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'zl-root',
   imports: [
     NavbarComponent,
-    FooterComponent
-  ],
+    FooterComponent,
+    RouterOutlet
+],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
