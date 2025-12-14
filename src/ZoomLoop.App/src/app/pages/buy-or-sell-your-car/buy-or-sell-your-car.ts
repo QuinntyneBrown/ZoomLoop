9b396f1ce6dacfc779d8fa9b8d490f98279f8f7d
Quinntyne Brown <quinntynebrown@gmail.com>
2025-12-14 13:15:25 -0500

    update

--- src/ZoomLoop.App/src/app/pages/buy-or-sell-your-car/buy-or-sell-your-car.ts
+++ src/ZoomLoop.App/src/app/pages/buy-or-sell-your-car/buy-or-sell-your-car.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SellingOrTrading } from "../../components";
 
 @Component({
   selector: 'app-buy-or-sell-your-car',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SellingOrTrading],
   templateUrl: './buy-or-sell-your-car.html',
   styleUrl: './buy-or-sell-your-car.scss'
 })
